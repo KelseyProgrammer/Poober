6d8ce09821d94bd3036ee28599ed05194ead5389
Christopher Ament <chrisament45@gmail.com>
2025-03-06 19:31:39 -0500

    Update database connection and fix vulnerabilities

--- .history/config/database_20250306190555.js
+++ .history/config/database_20250306190555.js
@@ -1,9 +1,13 @@
+// filepath: /Users/sarahandchris/repos/100DEVS/Poober/config/db.js
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_URI);
+        await mongoose.connect(process.env.DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('MongoDB connected...');
     } catch (err) {
         console.error(err.message);
